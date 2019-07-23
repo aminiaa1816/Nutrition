e3f99d76eb4f5cd59a38ebe293531cc4ab5ad9d4
aminiaa1816 <52106722+aminiaa1816@users.noreply.github.com>
2019-07-24 00:48:42 +0800

    Add files via upload

--- RecipeTest.java
+++ RecipeTest.java
@@ -2,11 +2,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 class RecipeTest {
 
+
+	private Ingredients ing = new Ingredients();
+
+
 	@Test
-	void testAppendToCookbook() {
-		fail("Not yet implemented"); // TODO
+	public void uploadRecipeFileTest() throws FileNotFoundException {
+
+		HashMap<String,Ingredients> uploadedRecipeFile=ing.uploadIngredientInformation();
+		
+		assertTrue(uploadedRecipeFile.get("45001530").long_name_1.equals("BARBECUE SAUCE"));
+		
 	}
+
 
 }
